f264661a49a1cb728b307a9e2112f3d4f43f8d87
Oscar Romeu <osca.romero@duocuc.cl>
2023-09-12 17:45:12 -0300

    Integracion de API mapbox

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public map: mapboxgl.Map;
+  public style = 'mapbox://styles/mapbox/streets-v11';
 
-  constructor() {}
+  constructor() {
+    this.map = null!;
+    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    mapboxgl.accessToken = 'pk.eyJ1Ijoib3MyNnJvbWV1IiwiYSI6ImNsbWZ0ZTduMDA4Y3gzc2xsd2xlaDV0bGoifQ.DgpMlL9vhYiUj9BrUjgYOg';
+  }
+
+  ngOnInit(){
+    this.buildMap();
+  }
+
+  buildMap(){
+    this.map = new mapboxgl.Map({
+      container: 'mapa-box',
+      style: this.style,
+      zoom: 14,
+      center: [
+        -99.2064496,
+        19.4050906
+      ]
+    });
+  }
 
 }
