238246a158c391d594a3542bdc230a3663516fa2
Raúl Llanos <rau.llanos@duocuc.cl>
2023-10-18 18:53:16 -0300

    Actualizando sign-in

--- src/app/pages/auth-screen/sign-in/sign-in.component.ts
+++ src/app/pages/auth-screen/sign-in/sign-in.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AlertController, IonicModule} from '@ionic/angular';
 import { ResetPasswordComponent } from '../reset-password/reset-password.component';
@@ -16,13 +16,22 @@ import { Router, NavigationExtras } from '@angular/router';
 })
 export class SignInComponent  implements OnInit {
 
-  form = new FormGroup ('');
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('');
+  // form = new FormGroup ('');
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  // password = new FormControl('');
+
   type = true;
 
 
-  constructor(private router: Router) { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+    ) { }
+
+  form = this.fb.group({
+    'email': [null, [Validators.required, Validators.email]],
+    'password': [null, [Validators.required, Validators.minLength(8)]]
+  })
 
   changeType() {}
 
